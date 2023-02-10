8e9b3e784bba20fa1dff4cae085fae1270e9c2ca
MG <minesim22@gmail.com>
2023-02-10 19:27:27 +0100

    minor changes~

--- public/js/main.js
+++ public/js/main.js
@@ -4,8 +4,10 @@ socket.on("message", message => {
     console.log(message);
 })
 
-/////
+
+//make move on the tic-tac-toe field
 function change(id)
 {
     document.getElementById(id).innerHTML = "X";
 }
+
--- server.js
+++ server.js
@@ -7,15 +7,22 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+
 //setting "public" as static folder
 app.use(express.static(path.join(__dirname,"public")));
 
+
 io.on("connection", socket => {
-    console.log("New WS Connection");
-
+    username="abc"
+    append_player_list(username);
     socket.emit("message", "Welcome");
-})
 
+    socket.on("disconnect", () => {
+
+    })
+
+
+})
 
 const PORT = 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
