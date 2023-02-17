43a29c23ed486033829bf13735e0043977a05e3f
MG <minesim22@gmail.com>
2023-02-17 03:44:40 +0100

    add dynamic room member list, display room name etc.

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,23 @@
 const socket = io();
 
 
-//to get username and room from url, i.e. from form
+//to get username and room from url, i.e. from the form
 const { username, room, role } = Qs.parse(location.search, {
     ignoreQueryPrefix: true,
 });
 //send username and url to the server
 socket.emit("joinRoom", {username,room,role})
 
+socket.on("roomName", (room) => {
+    displayRoomName(room);
+})
 
+//get and display room members
+socket.on("roomMembers", (users) => {
+    dispayRoomMembers(users);
+})
+
+//print recieved message to the console
 socket.on("message", message => {
     console.log(message);
 })
@@ -20,3 +29,18 @@ function change(id)
     document.getElementById(id).innerHTML = "X";
 }
 
+
+function displayRoomName(room) {
+    let title = document.getElementById("title");
+    title.innerHTML += "<h1>Room: " + room + "</h1><br>";
+}
+
+
+function dispayRoomMembers(users) {
+    console.log(users);
+    let ul = document.getElementById("playerList");
+    ul.innerHTML = "";
+    for (user in users) {
+        ul.innerHTML += "<li>" + users[user].username + "</li>";
+    }
+}
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
-const {userJoin, getCurrentUser} = require("./users");
+const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require("./users");
 
 const app = express();
 const server = http.createServer(app);
@@ -16,22 +16,32 @@ app.use(express.static(path.join(__dirname,"public")));
 io.on("connection", socket => {
 
     socket.on("joinRoom", ({username,room,role}) => {
-        console.log("New connection");
+        console.log("New connection");//log on the server
 
         const user = userJoin(socket.id,username,room,role); //create new user
         socket.join(user.room); //join room
         socket.emit("message", `Welcome ${username}`); //to the user
         socket.broadcast.to(user.room).emit("message", `${username} has joined the room as a ${role}`) //to all room members
-        //append_player_list(username);
-    })
+        
 
-    
+        //sending room member information to all room members
+        io.to(user.room).emit("roomMembers", users = getRoomUsers(user.room))
+        socket.emit("roomName",user.room);
+    })
     
 
     socket.on("disconnect", () => {
         console.log("Disconnection");
-    })
+        const user = userLeave(socket.id);
 
+        if(user) {
+            io.to(user.room).emit("message", `${user.role} ${user.username} has left the room`) //to all room members
+            //send updated user list
+            io.to(user.room).emit("roomMembers", {
+                users: getRoomUsers(user.room),
+            })
+        }
+    })
 
 })
 
